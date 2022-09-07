76be8f9f97ea5c1a3aa86454622b4c93e89bc196
heyimjustalex <21158649+heyimjustalex@users.noreply.github.com>
2022-09-07 19:20:55 +0200

    Configure Spring Boot

---
+++ backend/src/test/java/com/ctf/CTFtastic/CtFtasticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctf.CTFtastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtFtasticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
