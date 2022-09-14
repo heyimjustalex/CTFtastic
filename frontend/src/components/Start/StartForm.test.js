77f70d7bf77596e46a914c70f45a2a211c1642a9
heyimjustalex <21158649+heyimjustalex@users.noreply.github.com>
2022-09-14 15:25:21 +0200

    Add StartPage Tests

---
+++ frontend/src/components/Start/StartForm.test.js
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+// describe('StartForm component testing', () => {
+//     test('', () => {
+//         //Arrange
+//         render(<Greeting />)
+
+//         //Act
+//         //...noting
+
+//         //Assert
+//         const element = screen.getByText('hello world!', { exact: false });
+//         expect(element).toBeInTheDocument();
+
+//     });
+
+// }
+// )
---
+++ frontend/src/components/Start/StartPage.test.js
@@ -0,0 +1,55 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import StartPage from './StartPage';
+import userEvent from '@testing-library/user-event';
+
+describe('StartPage component', () => {
+    test('Renders CTF string', () => {
+        //Arrange
+        render(<StartPage />)
+
+        //Act
+        const element = screen.getByText('CTF', { exact: false });
+
+        //Assert
+        expect(element).toBeInTheDocument();
+
+    })
+
+    test('Renders tastic string', () => {
+        //Arrange
+        render(<StartPage />)
+
+        //Act
+        const element = screen.getByText('tastic', { exact: false });
+
+        //Assert
+        expect(element).toBeInTheDocument();
+    })
+
+    test('Renders Get started! button', () => {
+        //Arrange
+        render(<StartPage />)
+
+        //Act
+        const buttonElement = screen.getByRole('button');
+
+        //Assert
+        expect(buttonElement).toBeInTheDocument();
+
+    })
+
+    test('Calls function on button click', () => {
+        //Arrange
+        const onGetStarted = jest.fn();
+        render(<StartPage onGetStarted={onGetStarted} />)
+
+        //Act
+        const buttonElement = screen.getByRole('button');
+        fireEvent.click(buttonElement);
+
+        //Assert
+        expect(onGetStarted).toHaveBeenCalledTimes(1);
+
+    })
+});
+
