c44c3f3a06e2339db57a1a826adf8a70c70dde89
Unf0rgiv3n <jakub.kwia2@wp.pl>
2022-11-02 21:15:11 +0100

    add deployment and svc for operator

--- infra/challs_operator/chall_operator.py
+++ infra/challs_operator/chall_operator.py
@@ -1,14 +1,33 @@
 from flask import Flask, jsonify, request, make_response
-import kubernetes
+from kubernetes import client, config, utils
 import os
-import sys
 
 app = Flask(__name__)
+
+resources_name = 'resources.yaml'
 
 @app.route('/startchall', methods=['POST'])
 def run_team_challenge():
     test_json = request.get_json()
     #print(test_json, file=sys.stdout)
 
-    os.system("helm template ./teamchallenges > test.yaml")
-    return make_response('ok', 200)
+
+    os.system(f"helm template ./teamchallenges > {resources_name}")
+
+    #config.load_kube_config()
+    k8s_client = client.ApiClient()
+    utils.create_from_yaml(k8s_client,resources_name,verbose=True)
+
+    return make_response('Challenge created', 200)
+
+@app.route('/stopchall', methods=['DELETE'])
+def stop_team_challenge():
+    test_json = request.get_json()
+
+    os.system(f"helm template ./teamchallenges > {resources_name}")
+
+    config.load_kube_config()
+    k8s_client = client.ApiClient()
+    #utils.
+
+    return make_response('Challenge stopped', 200)
