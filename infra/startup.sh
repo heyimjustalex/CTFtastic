29e899cf770a4e4a643071d162f9b7c5a104bf94
Unf0rgiv3n <jakub.kwia2@wp.pl>
2022-11-12 01:44:19 +0100

    buildkit fix, operator new endpoints

--- infra/startup.sh
+++ infra/startup.sh
@@ -5,3 +5,4 @@ cd /home/docker/registry/certs
 openssl req \
  -x509 -newkey rsa:4096 -days 365 -config openssl.conf \
  -keyout certs/domain.key -out certs/domain.crt
+helm install ctftastic ./ctftastic
