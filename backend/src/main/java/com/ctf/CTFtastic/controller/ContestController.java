7a8a138f3045b44338b483bf6adc719e14a656ab
Grzegorz <GrzegoRes@gmail.com>
2022-11-12 21:24:29 +0100

    Update ContestController.java

--- backend/src/main/java/com/ctf/CTFtastic/controller/ContestController.java
+++ backend/src/main/java/com/ctf/CTFtastic/controller/ContestController.java
@@ -207,7 +207,7 @@ public class ContestController {
             //200
             //201
             //202
-            
+
             challengeService.updateBuild("done", id);
             Challenge challenge = challengeService.getChallange(id);
             Map<String, String> elements =  new HashMap<>();
